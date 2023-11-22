efce124172886928a89a12795d4916d333e64eae
Onergrand <saveliy.kuzmin@mail.ru>
2023-11-22 21:10:56 +0500

    updated gitignore

--- scripts/api.js
+++
@@ -1,7 +0,0 @@
-export function getOpenWeatherApiKey(latitude, longitude) {
-    return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=00b48d94481070eaee4d91619c5a0bbe`
-}
-
-export function getTimeApiKey() {
-    return 'http://worldtimeapi.org/api/timezone/Europe/London';
-}
